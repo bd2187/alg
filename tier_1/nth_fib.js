2dd5c25725b9dea455c6f538ea76471fa95666f4
bd2187 <brandon.dionisio.26@gmail.com>
2019-11-18 20:02:01 -0800

    Initial commit. Create GetFib

---
+++ tier_1/nth_fib.js
@@ -0,0 +1,16 @@
+/*
+    Nth Fibonacci
+
+    The Fibonacci sequence is defined as follows: the first number of sequence is 0, the second number is 1, and the nth number is the sum of the (n - 1)th and (n - 2)th numbers. Write a function that takes in an integer n and returns the nth Fibonacci number
+
+    Sample input: 6
+    Sample output: 5 (0, 1, 1, 2, 3, 5)
+*/
+
+const GetFib = {
+    recur: function(n) {
+        console.log(n);
+    }
+};
+
+module.exports = GetFib;
