6375bee93728f2f72b8ebdba00eca9f7abbce623
bd2187 <brandon.dionisio.26@gmail.com>
2019-11-20 19:26:57 -0800

    Initial commit. Find largest number in each array

---
+++ tier_1/largestOfFour.js
@@ -0,0 +1,13 @@
+/*
+    Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+    Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+*/
+
+const LargestOfFour = {
+    init: function(arr) {
+        return arr.map(subArr => Math.max(...subArr));
+    }
+};
+
+module.exports = LargestOfFour;
