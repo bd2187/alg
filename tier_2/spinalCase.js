9c70db6a800a6d44a00c7ec87d2c77fcd417712f
bd2187 <brandon.dionisio.26@gmail.com>
2020-03-31 19:00:59 -0700

    Solve SpinalCase

--- tier_2/spinalCase.js
+++ tier_2/spinalCase.js
@@ -3,5 +3,33 @@
 */
 
 const SpinalCase = {
-    init: function(str) {}
+    init: function(str) {
+        var arr = str
+            .replace(/_/g, "-")
+            .replace(/ /g, "-")
+            .split("");
+
+        var val = "";
+
+        arr.forEach((item, index, arr) => {
+            if (
+                item === item.toUpperCase() &&
+                index !== 0 &&
+                item !== "-" &&
+                arr[index - 1] !== "-"
+            ) {
+                val += `-${item.toLowerCase()}`;
+            } else if (item === "-") {
+                val += "-";
+            } else if (item === item.toUpperCase()) {
+                val += item.toLowerCase();
+            } else {
+                val += item;
+            }
+        });
+
+        return val;
+    }
 };
+
+module.exports = SpinalCase;
